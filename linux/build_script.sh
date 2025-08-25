d24a4dd767ef2ab12e73875b0d59d1ebdbd588c9
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2025-08-25 23:20:58 +0200

    Finish docker setup.

--- linux/build_script.sh
+++ linux/build_script.sh
@@ -1,1 +1,7 @@
-git clone https://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@github.com/TRDario/Bodge.git .
+#! /bin/sh
+
+git clone https://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@github.com/TRDario/Bodge.git
+cd Bodge
+../cmake/bin/cmake -G "Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang-20 -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++-20 -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -S . -B build
+../cmake/bin/cmake --build build --config Release --target all --
+cp -R build/final/* /output
