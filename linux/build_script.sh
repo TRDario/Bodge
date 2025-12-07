d908517495e4aa43d0fbe613487a29882ea4ec4e
TRDario <dario.cvitanovic04@gmail.com>
2025-12-07 17:16:59 +0100

    Fix linux built script.

--- linux/build_script.sh
+++ linux/build_script.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-git clone https://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@github.com/TRDario/Bodge.git
+git clone https://github.com/TRDario/Bodge.git
 cd Bodge
 ../cmake/bin/cmake -G "Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang-20 -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++-20 -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -S . -B build
 ../cmake/bin/cmake --build build --config Release --target all --
